5f1faeb1e08659e8866acf5057e2cb7d7671cd61
woo3145 <lcwoo3145@gmail.com>
2024-05-10 23:23:36 +0900

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,26 +16,37 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Vertical Heatmap'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  late Map<DateTime, int> datasets = {};
+
+  @override
+  void initState() {
+    super.initState();
+    var now = DateTime.now();
+    datasets[DateTime(now.year, now.month, now.day)] = 1;
+    datasets[DateTime(now.year, now.month, now.day - 1)] = 2;
+    datasets[DateTime(now.year, now.month, now.day - 2)] = 3;
+    datasets[DateTime(now.year, now.month, now.day - 3)] = 4;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Flutter Vertical Heatmap'),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -48,29 +59,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 endDate: DateTime.now(),
                 monthLabel: HeatMapDefault.monthLabel,
                 weekLabel: HeatMapDefault.weekLabel,
-                datasets: {
-                  DateTime(2024, 5, 6): 3,
-                  DateTime(2024, 5, 7): 7,
-                  DateTime(2024, 5, 8): 10,
-                  DateTime(2024, 5, 9): 13,
-                  DateTime(2024, 5, 5): 6,
-                },
+                datasets: datasets,
                 size: 32,
                 margin: const EdgeInsets.all(3),
                 colorsets: {
                   1: Colors.green.shade100,
-                  4: Colors.green.shade300,
-                  8: Colors.green.shade500,
-                  12: Colors.green.shade700,
+                  2: Colors.green.shade300,
+                  3: Colors.green.shade500,
+                  4: Colors.green.shade700,
                 },
                 onClick: (value) {
+                  setState(() {
+                    datasets[value] =
+                        datasets[value] != null ? datasets[value]! + 1 : 1;
+                  });
                   ScaffoldMessenger.of(context).clearSnackBars();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(value.toString()),
-                      duration: const Duration(seconds: 1),
-                    ),
-                  );
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(value.toString())));
                 },
               ),
               const SizedBox(height: 80),
